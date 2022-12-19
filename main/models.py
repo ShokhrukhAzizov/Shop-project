c654344bba7471e30f1b11b583bbc27f46b1fd17
DADAJONOVB <dadajonovboburjon3@gmail.com>
2022-12-19 10:46:25 +0300

    review model

--- main/models.py
+++ main/models.py
@@ -18,11 +18,38 @@ class Product(models.Model):
     is_sold = models.BooleanField(default=False)
     review = models.SmallIntegerField()
 
+    def __str__(self):
+        return self.title
+
+class ProductReview(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        if self.pk is None:
+            raitings = ProductReview.objects.filter(product=self.product)
+            raiting_quantity = raitings.count()+1
+            raitings_rw = 0
+            for i in raitings:
+                raitings_rw += i.rating
+            raitings_rw += self.rating
+            current = raitings_rw/raiting_quantity
+
+            product = self.product
+            product.review = current
+            product.save()
+            Product.save(self, *args, **kwargs)
+
+
 class Card(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     product = models.ForeignKey(Product,on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=9,decimal_places=2)
     is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.user.username
 
 class Order(models.Model):
     card = models.ForeignKey(Card,on_delete=models.CASCADE)
@@ -30,16 +57,24 @@ class Order(models.Model):
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.user.username
+
 class Reserved(models.Model):
     card = models.ForeignKey(Card,on_delete=models.CASCADE)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     is_active= models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.user.username
+
 class Wishlist(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     product = models.ForeignKey(Product,on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.user.username
 
 
 class Company(models.Model):
@@ -49,7 +84,13 @@ class Company(models.Model):
     email = models.EmailField()
     social_media = models.TextField()
 
+    def __str__(self):
+        return self.about_us
+
 class Message(models.Model):
     name = models.CharField(max_length=150)
     body = models.TextField()
     email = models.EmailField()
+
+    def __str__(self):
+        return self.name
